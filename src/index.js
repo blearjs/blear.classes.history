62b87bb936f8e089d860721d890542839296e409
#云淡然 <cloudcome@163.com>
2018-11-15 18:57:57 +0800

    ~ history class

--- src/index.js
+++ src/index.js
@@ -7,6 +7,145 @@
 
 'use strict';
 
+var Events = require('blear.classes.events');
+var object = require('blear.utils.object');
 
-module.exports = 'index';
+var defaults = {
+    maxLength: 100
+};
+var History = Events.extend({
+    className: 'History',
+    constructor: function (options) {
+        var the = this;
+
+        the[_options] = object.assign({}, defaults, options);
+        History.parent(the);
+        the[_histories] = [];
+        the.length = 0;
+        the[_current] = -1;
+    },
+
+    /**
+     * 新增历史
+     * @param item
+     * @returns {History}
+     */
+    push: function (item) {
+        var the = this;
+
+        // 不在记录末尾
+        if (the[_current] !== the.length - 1) {
+            the[_histories].splice(
+                the[_current] + 1,
+                the.length - the[_current] - 1
+            );
+            the.length = the[_current] + 1;
+        }
+
+        if (the.length === the[_options].maxLength) {
+            the[_histories].shift();
+            the.length--;
+            the[_current]--;
+        }
+
+        the[_histories].push(item);
+        the.length++;
+        the[_current]++;
+        return the;
+    },
+
+    /**
+     * 替换历史
+     * @param item
+     * @returns {History}
+     */
+    replace: function (item) {
+        var the = this;
+
+        if (the[_current] === -1) {
+            the[_histories].push(item);
+            the.length++;
+            the[_current]++;
+        } else {
+            the[_histories][the[_current]] = item;
+        }
+
+        return the;
+    },
+
+    /**
+     * 获取当前激活的记录
+     * @returns {*}
+     */
+    active: function () {
+        var the = this;
+
+        if (the[_current] === -1) {
+            return null;
+        }
+
+        return the[_histories][the[_current]];
+    },
+
+    /**
+     * 前进
+     * @returns {*}
+     */
+    forward: function () {
+        var the = this;
+
+        switch (the[_current]) {
+            case -1:
+                return null;
+
+            case the.length - 1:
+                return the.active();
+
+            default:
+                the[_current]++;
+                return the.active();
+        }
+    },
+
+    /**
+     * 后退
+     * @returns {*}
+     */
+    back: function () {
+        var the = this;
+
+        switch (the[_current]) {
+            case -1:
+                return null;
+
+            case 0:
+                return the.active();
+
+            default:
+                the[_current]--;
+                return the.active();
+        }
+    },
+
+    /**
+     * 跳转到指定点
+     * @param index
+     * @returns {*}
+     */
+    go: function (index) {
+        var the = this;
+        the[_current] = index;
+        return the.active();
+    }
+});
+var proto = History.prototype;
+var sole = History.sole;
+var _options = sole();
+var _histories = sole();
+var _current = sole();
+
+History.defaults = defaults;
+module.exports = History;
+
+
 
--- test/test.index.js
+++ test/test.index.js
@@ -8,13 +8,152 @@
 'use strict';
 
 var expect = require('chai-jasmine').expect;
-var index = require('../src/index.js');
+var History = require('../src/index.js');
 
 describe('blear.classes.history', function () {
 
-    it('exports', function () {
-        expect(index).toEqual('index');
+    it('先 push', function () {
+        var history = new History();
+
+        expect(history.active()).toBe(null);
+        expect(history.length).toBe(0);
+
+        history.push('A');
+        expect(history.active()).toBe('A');
+        expect(history.length).toBe(1);
+
+        history.replace('B');
+        expect(history.active()).toBe('B');
+        expect(history.length).toBe(1);
     });
 
+    it('先 replace', function () {
+        var history = new History();
+
+        expect(history.active()).toBe(null);
+        expect(history.length).toBe(0);
+
+        history.replace('A');
+        expect(history.active()).toBe('A');
+        expect(history.length).toBe(1);
+
+        history.replace('B');
+        expect(history.active()).toBe('B');
+        expect(history.length).toBe(1);
+    });
+
+    it('-1 前进', function () {
+        var history = new History();
+
+        expect(history.active()).toBe(null);
+        expect(history.length).toBe(0);
+
+        expect(history.forward()).toBe(null);
+        expect(history.length).toBe(0);
+    });
+
+    it('末尾前进', function () {
+        var history = new History();
+
+        history.push('A');
+        expect(history.forward()).toBe('A');
+    });
+
+    it('-1 后退', function () {
+        var history = new History();
+
+        expect(history.active()).toBe(null);
+        expect(history.length).toBe(0);
+
+        expect(history.back()).toBe(null);
+        expect(history.length).toBe(0);
+    });
+
+    it('起点后退', function () {
+        var history = new History();
+
+        history.push('A');
+        expect(history.back()).toBe('A');
+    });
+
+    it('指定移动', function () {
+        var history = new History();
+
+        history.push('A');    // -->0
+        history.push('B');    // -->1
+        history.push('C');    // -->2
+        history.replace('D'); // ---->2
+        history.replace('E'); // ------->2
+        history.push('F');    // ------->3
+
+        expect(history.length).toBe(4);
+        expect(history.active()).toBe('F');
+
+        expect(history.go(0)).toBe('A');
+        expect(history.forward()).toBe('B');
+        expect(history.back()).toBe('A');
+
+        expect(history.go(2)).toBe('E');
+        expect(history.forward()).toBe('F');
+        expect(history.back()).toBe('E');
+
+        expect(history.go(1)).toBe('B');
+        expect(history.forward()).toBe('E');
+        expect(history.back()).toBe('B');
+
+        expect(history.go(3)).toBe('F');
+        expect(history.forward()).toBe('F');
+        expect(history.back()).toBe('E');
+    });
+
+    it('中途 push', function () {
+        var history = new History();
+
+        history.push('A');    // -->0
+        history.push('B');    // -->1
+        history.push('C');    // -->2
+        history.replace('D'); // ---->2
+        history.replace('E'); // ------->2
+        history.push('F');    // ------->3
+
+        history.go(1);
+        // A --> B --> G
+        history.push('G');
+        expect(history.length).toBe(3);
+        expect(history.active()).toBe('G');
+    });
+
+    it('中途 replace', function () {
+        var history = new History();
+
+        history.push('A');    // -->0
+        history.push('B');    // -->1
+        history.push('C');    // -->2
+        history.replace('D'); // ---->2
+        history.replace('E'); // ------->2
+        history.push('F');    // ------->3
+
+        history.go(1);
+        // A --> B --> G --> F
+        history.replace('G');
+        expect(history.length).toBe(4);
+        expect(history.active()).toBe('G');
+    });
+
+    it('最大长度', function () {
+        var history = new History({
+            maxLength: 3
+        });
+
+        history.push('A'); // >>>> drop
+        history.push('B');
+        history.push('C');
+        history.push('D');
+
+        expect(history.length).toBe(3);
+        expect(history.go(0)).toBe('B');
+        expect(history.go(1)).toBe('C');
+        expect(history.go(2)).toBe('D');
+    });
 });
 
